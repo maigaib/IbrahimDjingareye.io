d00b22fc1aa917c49f0110bb06cfb8b4fd903b12
Ibrahim Djingareye <maigaib82@gmail.com>
2023-12-29 03:31:44 +0000

    form work

--- script.js
+++ script.js
@@ -55,13 +55,35 @@ sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
 /*=================================== titres ========================================================== */
-const typed = new Typed(".multiple-text", {
-    strings: ["Développeur Web et Mobile", "Designer Web", "Administrateur Systèmes et <br> Réseaux", "Concepteur de base de <br> données"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    typeDelay: 1000,
-    loop: true
-});
+// const typed = new Typed(".multiple-text", {
+//     strings: ["Développeur Web et Mobile", "Designer Web", "Administrateur Systèmes et <br> Réseaux", "Concepteur de base de <br> données"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     typeDelay: 1000,
+//     loop: true
+// });
 // =========================================================================================================
+function SendEmail(){
+
+    let nom =document.getElementById("nom").value;
+    let email =document.getElementById("email").value;
+    let message =document.getElementById("message").value;
+    let contact =document.getElementById("contact").value;
+
+    let body = "Nom: " + nom + "<br/> Email: " + email + "<br/> Contact: " + contact + "<br/> Message: <br/>" + message;
+
+    Email.send({
+    SecureToken : "054d8fdf-9931-4a22-8a88-ec8afbbf91c2",
+    To : 'maigaib82@gmail.com',
+    From : "maigaib82@gmail.com",
+    Subject : "Message de mon Portfolio",
+    Body : body
+    }).then(
+    message => alert(message)
+    );
+
+    document.getElementById("contactForm").reset();
+
+}
 
 
