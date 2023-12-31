cb3d49c0ba95fde19a8708491007158749589b74
Ibrahim Djingareye <maigaib82@gmail.com>
2023-12-31 01:36:28 +0000

    okok

--- script.js
+++ script.js
@@ -63,54 +63,62 @@ sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200});
 //     loop: true
 // });
 // =========================================================================================================
-function SendEmail(){
-
-    let nom =document.getElementById("nom").value;
-    let email =document.getElementById("email").value;
-    let message =document.getElementById("message").value;
-    let tel = document.getElementById("tel").value;
-
-    if (!nom || !email || !message || !tel) {
-        alert("Veuillez remplir tous les champs.");
-        // return;
+function SendEmail() {
+    // Obtention des valeurs du formulaire
+    const nom = document.getElementById("nom").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    const tel = document.getElementById("tel").value;
+  
+    // Validation des champs obligatoires
+    if (!nom || !email || !message) {
+      alert("Veuillez remplir tous les champs.");
+      return; // Sortie de la fonction si un champ est vide
     }
-        var emailInput = document.getElementById('email');
-        var emailError = document.getElementById('emailError');
-        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (emailRegex.test(email)) {
-        emailError.textContent = '';
-      } else{
-        emailError.textContent = 'Veuillez saisir une adresse e-mail valide.';
-        // alert("Veuillez saisir une adresse e-mail valide.");
-        // return;
+  
+    // Validation du format de l'adresse e-mail
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      document.getElementById("emailError").textContent = "Veuillez saisir une adresse e-mail valide.";
+      return; // Sortie si l'adresse e-mail est invalide
+    } else {
+      document.getElementById("emailError").textContent = ""; // Réinitialisation du message d'erreur
     }
-      var telInput = document.getElementById('tel');
-      var telError = document.getElementById('telError');
-      var regexPhone = /^\+(?:[0-9] ?){6,14}[0-9]$/;
-
-      if (regexPhone.test(telInput.value)) {
-        // Le numéro de téléphone est valide
-        telError.textContent = '';
-      } else {
-        // Le numéro de téléphone est invalide
-        telError.textContent = 'Veillez saisir un numéro valide';
-      }
-    let body = "Nom: " + nom + "<br/> Email: " + email + "<br/> Contact: " + tel + "<br/> Message: <br/>" + message;
-    console.log(body);
-
+  
+    // Validation du format du numéro de téléphone (obligatoire)
+    const regexPhone = /^(\+\d{1,3}\s?)?(\d{8}|\d{1,14})$/;
+    if (!regexPhone.test(tel)) {
+      document.getElementById("telError").textContent = "Veuillez saisir un numéro de téléphone valide (8 chiffres ou format international).";
+      return; // Sortie si le numéro de téléphone est invalide
+    } else {
+      document.getElementById("telError").textContent = ""; // Réinitialisation du message d'erreur
+    }
+  
+    // Construction du corps de l'e-mail
+    const body = `Nom: ${nom} <br/> Email: ${email} <br/> Contact: ${tel} <br/> Message: <br/> ${message}`;
+  
+    // Envoi de l'e-mail
     Email.send({
-    SecureToken : "054d8fdf-9931-4a22-8a88-ec8afbbf91c2",
-    To : 'maigaib82@gmail.com',
-    From : "maigaib82@gmail.com",
-    Subject : "Message de mon Portfolio",
-    Body : body
+      SecureToken: "054d8fdf-9931-4a22-8a88-ec8afbbf91c2",
+      To: "maigaib82@gmail.com",
+      From: "maigaib82@gmail.com",
+      Subject: "Message de mon Portfolio",
+      Body: body
     }).then(
-    message => alert(message)
+      message => {
+        alert("E-mail envoyé avec succès.");
+        console.log(message);
+      },
+      error => {
+        console.error("Erreur d'envoi de l'e-mail:", error);
+        alert("Une erreur s'est produite lors de l'envoi de l'e-mail. Veuillez réessayer.");
+      }
     );
-
+  
+    // Réinitialisation du formulaire
     document.getElementById("contactForm").reset();
-
-}
+  }
+  
 // function isValidEmail(email) {
 //     // Expression régulière pour valider une adresse e-mail
 //     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
